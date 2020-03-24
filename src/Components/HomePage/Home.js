deec9091960dc9ed164c04a39ab4c3ae8eb18dea
Alan <alan@Alans-MacBook-Pro.local>
2020-03-24 19:55:48 +0530

    fixed nav-bar

--- src/Components/HomePage/Home.js
+++ src/Components/HomePage/Home.js
@@ -21,7 +21,7 @@ const Home =()=> {
             
              <div>
              <NavbarTop/>
-             <Carousel/> 
+             {/* <Carousel/>  */}
              </div>
        
         
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,40 +1,43 @@
 import React, { useContext } from 'react'
 import './styles.css'
 
+
 import LogIn from '../Login/LogIn'
 import LogOut from '../LogOut'
 import {UserContext} from '../../Contexts/UserContext'
 import { Link } from 'react-router-dom'
 
 export default function Navbar() {
+    const navIcon=()=>{
+        const  navLinks=document.querySelector(".nav-links");
+        navLinks.classList.toggle("open");
+
+    }
     const user=useContext(UserContext);
     return (
-        <div className="navbar-fixed" style={{background:'coral'}}>
-        <div className="">
-            <nav className=" z-depth-0 transparent">
-                <div className="nav-wrapper" id="navbar">
-                <a href="#!" className="brand-logo ">Our Happy Wedding</a>
-                <ul className="right hide-on-med-and-down">
-                    <li> <Link to="/about">About Us</Link> </li>
-                    
-                    <li> <Link to="/contact">Contact Us</Link> </li>
-                    
-                    {!user.loggedIn? (
-                            <li><Link to="/login">Log In</Link></li>
-                        ):( 
-                            <span className="setp">
-                            <li> <span className="setp">Hi, {user.email} </span></li>
-                            <li onClick={user.requestLogout} className="active"><span className="setp"> Log Out</span> </li>
-                           </span>
-                            
-                            
-                        ) } 
-                    
-                    
-                </ul>
-                </div>
-            </nav>
-            </div>
+        <div className="nav-bar " >
+            <div className="nav-logo">Our Happy Wedding</div>
+           <ul className="nav-links">
+               <l1 className="nav-link"><a href="#">Home</a> </l1>
+               <l1 className="nav-link"><a href="#">About</a> </l1>
+               <l1 className="nav-link"><a href="#">Contact</a> </l1>
+               <li className="nav-link"><Link to="/login">Log In</Link></li>
+           </ul>
+           <div className="nav-icon" onClick={navIcon}><img src={require('../../resources/icons/nav.png')}alt="-"/> </div>
+            
         </div>
     )
 }
+
+
+
+// {!user.loggedIn? (
+//     <li><Link to="/login">Log In</Link></li>
+// ):( 
+//     <span className="">
+//     <li> <span className="">Hi, {user.email} </span></li>
+//     <li onClick={user.requestLogout} className="active"><span className="setp"> Log Out</span> </li>
+//    </span>
+    
+    
+// ) } 
